193e266d73205c8174ad02d0f9868fc9e753d6d7
Alex S Grimes <alex@alexgrimes.com>
2020-04-13 20:56:35 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do 
     array.include?(value_to_find)
-    return value_to_find(i)
+    return value_to_find(index)
     counter =+ 1 
   end 
 end
